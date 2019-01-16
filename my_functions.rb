983aa4aff3103f258390d55b960a3acf6113547a
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-01-16 15:41:06 +0000

    test two passed

--- my_functions.rb
+++ my_functions.rb
@@ -1,9 +1,22 @@
 def add_array_lengths(array_1, array_2)
   total_number = 0
-  
+
   total_number += array_1.length
   total_number += array_2.length
   return total_number
 
 
+end
+
+
+def sum_array(numbers)
+
+  total_number = 0
+
+  for number in numbers
+    total_number += number
+
+  end
+
+  return total_number
 end
