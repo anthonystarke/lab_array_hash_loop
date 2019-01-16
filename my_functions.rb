d103870f0f3704bab84d30eb1ded0236a931844b
Anthony Starke <32094574+anthonystarke@users.noreply.github.com>
2019-01-16 16:37:51 +0000

    Update my_functions.rb

--- my_functions.rb
+++ my_functions.rb
@@ -1,29 +1,17 @@
 def add_array_lengths(array_1, array_2)
-  total_number = 0
-
-  total_number += array_1.length
-  total_number += array_2.length
-  return total_number
-
-
+  return array_1.length + array_2.length
 end
 
 def sum_array(numbers)
-
   total_number = 0
-
   for number in numbers
     total_number += number
-
   end
-
   return total_number
 end
 
 def find_item(array, name)
-
   for item in array
-
     if item == name
       return true
     end
@@ -31,8 +19,6 @@ def find_item(array, name)
   return false
 end
 
-
 def get_first_key(wallets)
-  return wallets.keys[0]
-
+  return wallets.keys().first()
 end
